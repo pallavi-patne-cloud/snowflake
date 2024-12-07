cd54f3f5035a44a421025114fcf4980a0efcc6fa
pallavi-patne-cloud <patne.pallavi@gmail.com>
2024-12-07 15:54:03 +0530

    My third commit

---
+++ src/src/utils/snowUtils.py
@@ -0,0 +1,50 @@
+import snowflake.connector
+
+class SnowflakeConnector:
+    def __init__(self,account,user,password,warehouse,database,schema,role):
+        self.account = account
+        self.user = user
+        self.password = password
+        self.warehouse = warehouse
+        self.database = database
+        self.schema = schema
+        self.role = role
+    def connect(self):
+        self.connection = snowflake.connector.connect(
+            user = self.user,
+            password = self.password,
+            account = self.account,
+            warehouse = self.warehouse,
+            database = self.database,
+            schema = self.schema   
+
+        )
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
+
+account = "TNWYUNB-GF82581"
+user = "PALLAVI"
+password = "Ayaan2012"
+database = "SNOWFLAKE_SAMPLE_DATA"
+schema = "TPCH_SF1"
+role = "SYSADMIN"
+warehouse = "COMPUTE_WH"
+
+sf_connector = SnowflakeConnector(
+    account,user,password,warehouse,database,schema,role
+
+)
+sf_connector.connect()
+
+query = "select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.SUPPLIER LIMIT 10"
+result = sf_connector.execute_query(query)
+print(result)
+
+sf_connector.close_connection()
